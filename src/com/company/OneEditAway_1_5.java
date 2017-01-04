2621da15c20c96de9c93071ce92ca12f3f85bb67
J-Cordova <jordan.m.cordova@gmail.com>
2017-01-03 20:24:17 -0500

    Added Chapter 1 Problem 5 implementation and Unit Test

--- src/com/company/OneEditAway_1_5.java
+++ src/com/company/OneEditAway_1_5.java
@@ -1,6 +1,33 @@
 package com.company;
 
+import java.util.HashSet;
 
 public class OneEditAway_1_5
 {
+    public boolean isStringOneChangeAway(String a, String b)
+    {
+        if (Math.abs(a.length() - b.length()) > 1) return false;
+
+        HashSet<Character> aChars = new HashSet();
+
+        for(char c: a.toCharArray())
+        {
+            aChars.add(c);
+        }
+
+        int diffCount = 0;
+
+        for(char character : b.toCharArray())
+        {
+            if (!aChars.contains(character))
+            {
+                diffCount++;
+                if (diffCount > 1)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- test/OneEditAway_1_5_Tests.java
+++ test/OneEditAway_1_5_Tests.java
@@ -1,4 +1,30 @@
+import com.company.OneEditAway_1_5;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class OneEditAway_1_5_Tests
 {
+    final private String masterString = "bye";
+    final private String oneEditAway = "be";
+    final private String multiEditAway = "byod";
+
+    @Test
+    public void givenOneEditAwayString_IsStringOneChangeAway_ReturnsTrue()
+    {
+        OneEditAway_1_5 oneEditValidator = new OneEditAway_1_5();
+
+        boolean result = oneEditValidator.isStringOneChangeAway(masterString,oneEditAway);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenMultiEditAwayString_IsStringOneChangeAway_ReturnsFalse()
+    {
+        OneEditAway_1_5 oneEditValidator = new OneEditAway_1_5();
+
+        boolean result = oneEditValidator.isStringOneChangeAway(masterString,multiEditAway);
+
+        Assert.assertFalse(result);
+    }
 }
