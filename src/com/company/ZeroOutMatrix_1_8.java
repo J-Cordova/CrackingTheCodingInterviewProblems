bee8fb56e9f5787ce4e1f2b0e453ca8ed37d2b33
J-Cordova <jordan.m.cordova@gmail.com>
2017-01-06 02:57:02 -0500

    Added Chapter 1 Problem 8 Problem and Unit Test Classes

---
+++ src/com/company/ZeroOutMatrix_1_8.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class ZeroOutMatrix_1_8
+{
+    public int[][] zeroOutMatrix(int[][] matrix)
+    {
+        return matrix;
+    }
+}
---
+++ test/ZeroOutMatrix_1_8_Tests.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ZeroOutMatrix_1_8_Tests
+{
+    public void Given2x2MatrixWithZeros_ZeroOutMatrix_ReturnsMatrixWithRowColumnsZeroedOut()
+    {
+
+    }
+
+    public void Given3x3MatrixWithZeros_ZeroOutMatrix_ReturnsMatrixWithRowColumnsZeroedOut()
+    {
+
+    }
+}
